9db4f55a5d6f0f7e2aac2dc9fe36456d25d79ae1
chenw <wenchen.mails@gmail.com>
2024-05-05 20:45:30 +0800

    feat(gpio中断实现): 上传GPIO中断实验代码

---
+++ int/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @author wenchen (wenchen.mails@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-05-04
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+int main(int argc, char const* argv[])
+{
+    while (1) {
+        ;
+    }
+
+    return 0;
+}
